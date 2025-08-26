07fbdad50fed4f16658943cd5ec43f00f20da7f5
Robert Schmidt <robert@robschmidt.info>
2025-08-26 10:57:03 +0200

    update

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -14,8 +14,8 @@ export const Home = () => {
             Robert Schmidt
           </h1>
 
-          <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            I'm a developer with 5+ years of experience in React. <br/> Backed by strong skills in Python and PHP, I can develop and contribute to cutting-edge systems of many kinds, including Agentic AI Applications.
+          <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+            I'm a Full Stack Developer - mainly experienced (5+ years) in React. <br/> Backed by strong skills in Python and PHP, I can develop and contribute to cutting-edge systems of many kinds, including Agentic AI Applications.
           </p>
           <div className="flex justify-center space-x-4">
             <a
